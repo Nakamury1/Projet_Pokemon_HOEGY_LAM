fb0a451da253dbb3d0d631a072537f1572700e5f
jjuulliieee <julie.hoegy607@gmail.com>
2024-12-08 23:42:45 +0100

    first commit

---
+++ pokemon.js
@@ -0,0 +1,11 @@
+//Carroussel nouvelles cartes
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 0,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
